3ecf5d19d78c6eb375fd2030e800e9b38b451e70
Pasha <pavel8lisenkov@yandex.ru>
2024-01-04 02:01:11 +0400

    1

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let res = [];
+  for (let i = start; i < end; i++) {
+    res.push(i);
+  }
+  return res;
 }
 
 /**
